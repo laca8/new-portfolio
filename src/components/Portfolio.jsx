8f0ca42736abfb3f04c8c25448d564286660ebbb
Ragab <89777423+laca8@users.noreply.github.com>
2024-09-03 09:49:05 +0300

    Update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,6 +6,7 @@ import project4 from "../assets/4.png";
 import project5 from "../assets/5.png";
 import project6 from "../assets/6.png";
 import project7 from "../assets/7.png";
+import project8 from "../assets/8.png";
 import project9 from "../assets/9.png";
 import project10 from "../assets/10.png";
 import arrow from "../assets/arrow.png";
@@ -81,7 +82,7 @@ const projects = [
         },
       },
     {
-        img: "",
+        img: project8,
         title: "Project #8",
         description:
           "Build by using ReactJS",
